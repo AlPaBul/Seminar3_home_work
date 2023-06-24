74c4c8ac80c63aaa07aac070f60be524d78cc31d
Алексей Парасненко <aleksejparasnenko@MacBook-Pro-Aleksej.local>
2023-06-24 08:09:12 +0700

    задача 19

---
+++ HOMEWORKSEMINAR3/zadacha19/Program.cs
@@ -0,0 +1,75 @@
+﻿/* Урок 3. Базовые алгоритмы. Продолжение
+Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+
+Console.WriteLine("введите любое пятизначное число: ");
+
+int number1 (int numb )
+{
+    int result = numb;
+    string i = Convert.ToString(numb);
+    int leng = i.Length;
+    //Console.WriteLine(leng);
+
+ while (leng < 4 || leng > 6)
+{
+    Console.WriteLine("вы вели не верное число");
+    break;
+}
+    if (i[0]== i[4] && i[1] == i[3])
+    {
+        Console.WriteLine("число палиндром");
+    }
+    else {
+        Console.WriteLine("число не палиндром");
+    }
+    return result;   
+}
+
+int res = number1(int.Parse(Console.ReadLine()));
+Console.WriteLine(res);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+string i = Convert.ToString(res);
+int leng = i.Length;
+Console.WriteLine(leng);
+
+if (leng <= 4)
+{
+    Console.WriteLine("вы вели число меньше пяти символов");
+}
+if (leng >= 6)
+{
+    Console.WriteLine("вы вели число больше 5 цифр");
+}
+
+if (i[0] == i[4] && i[1] == i[3])
+{
+    Console.WriteLine("+");
+}
+else{
+    Console.WriteLine("-");
+}
+*/
---
+++ zadacha21/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+/* 
+/*Расстояние между точками — это число, равное корню из суммы квадратов разностей координат точек или длине разности радиус-векторов точек.*/
+ 
+
+
+decimal rr(decimal rrA)
+{
+
+decimal rezultat = rrA; 
+decimal rrA = ax + ay + az;
+Console.WriteLine("Введите координаты точки х: ");
+decimal ax = decimal.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты точки y: ");
+decimal ay = decimal.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты точки z: ");
+decimal az = decimal.Parse(Console.ReadLine());
+
+Console.WriteLine($"{rezultat}");
+
+return rezultat;
+
+}
+
+string gg = rr(rrA);
+Console.WriteLine(gg);
