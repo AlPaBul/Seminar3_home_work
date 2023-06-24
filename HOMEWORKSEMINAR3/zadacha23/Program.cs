96570fe30e9349d6fb7126996012d91a08f63cc4
Алексей Парасненко <aleksejparasnenko@MacBook-Pro-Aleksej.local>
2023-06-24 08:59:41 +0700

    задача 23

---
+++ HOMEWORKSEMINAR3/zadacha23/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+
+Console.WriteLine("введите любое число: ");
+double number = Convert.ToDouble(Console.ReadLine());
+int stepen = 3;
+
+double number1 = number/number;
+
+while (number1 <= number)
+{
+    number1 = number1 + 1;
+    Console.WriteLine(Math.Pow(number1, stepen));
+}
